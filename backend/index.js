b32983ab9e1e06a535cdc80ea4e664fc605523ee
Santosh Kumar <sk8065788@gmail.com>
2025-06-30 14:08:37 +0530

    fix the bugs in caninkart

--- backend/index.js
+++ backend/index.js
@@ -12,13 +12,13 @@ const adminRoutes = require("./Routes/Admin");
 const blogRoutes = require("./Routes/blogRoutes");
 const contactRoutes = require("./Routes/contactRoutes");
 const cookieParser = require("cookie-parser");
-
+const path = require('path');
 require("./Models/db");
 
 //middleware
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: [process.env.FRONTEND_URL, 'https://www.caninkart.com', 'https://caninkart.com'],
     methods: ["POST", "GET", "PUT", "DELETE"],
     credentials: true,
   })
@@ -37,7 +37,7 @@ app.use("/api/admin", adminRoutes);
 app.use("/api/blogs", blogRoutes);
 app.use("/api/cnt", contactRoutes);
 
-app.use("/uploads", express.static("uploads"));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 app.get("/", (req, res) => {
   res.send("hello ji");
