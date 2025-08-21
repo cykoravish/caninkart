a5d9616f13e37de3a7174f5c5d1aa164aa200fa0
Santosh Kumar <sk8065788@gmail.com>
2025-08-21 18:11:42 +0530

    ch

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -64,7 +64,7 @@ const router = createBrowserRouter([
       // { path: 'CountryPage', element: <CountryPage /> }
       {
         path: '*',
-        element: <HomePage />
+        element: <NotFound />
       }
     ],
   },
